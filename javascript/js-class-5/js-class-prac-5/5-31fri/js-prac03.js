baeb627a87365224db0553d9cfb76f15d957c4d2
bigkoooo <kimminseok8910@gmail.com>
2024-06-03 12:25:23 +0900

    study wisely

---
+++ javascript/js-class-5/js-class-prac-5/5-31fri/js-prac03.js
@@ -0,0 +1,23 @@
+
+
+// for
+//  (let j = 0; j <= 10000; j++) {
+//     if ( j%13==0 && j%2==1) {
+//         console.log(j);
+//     };
+//  }
+
+let sum = 0;
+
+ for
+ (let j = 0; j <= 100; j++) {
+    if ( j%2==0 || j%5==0) {
+        sum +=j
+    };
+    
+    
+} ;
+
+console.log(sum);   
+
+
