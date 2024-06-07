0f560a52b4a0fff47035dd6409677b02cda2f06c
bigkoooo <kimminseok8910@gmail.com>
2024-06-07 10:04:06 +0900

    do you know coding?

--- Tstory_example/innerHTML.js
+++ Tstory_example/innerHTML.js
@@ -7,7 +7,20 @@ const p4 = document.createElement('p');
 
 p2.innerText = '<b>p2</b>';
 p3.innerHTML = '<b>p3</b>';
-p4.textContent = 'p4';
+p4.textContent = '<b>p4</b>';
 
 
 container.append(p2,p3,p4); // 여러개 추가 가능!
+
+
+const container2 = document.querySelector('.container2')
+
+
+console.log(container2.innerText);
+  
+
+console.log(container2.innerHTML);
+  
+
+console.log(container2.textContent);
+  
