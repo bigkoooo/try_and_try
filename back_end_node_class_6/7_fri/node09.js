7005a9ff7dd984cbd77400823d0836f76d15b043
bigkoooo <kimminseok8910@gmail.com>
2024-06-07 18:59:33 +0900

    nodeJs.study

---
+++ back_end_node_class_6/7_fri/node09.js
@@ -0,0 +1,32 @@
+const http=require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    let path = "./views/"
+    switch(req.url){
+        case'/':
+            path +='node08.html'; //./view/node08.html
+            res.statusCode =200;
+            break;
+        case'/about':
+            path +='about.html';
+            res.statusCode =200;
+            break;
+        default:
+            res.statusCode =404;
+            break;
+    }
+
+    fs.readFile(path, (err, data)=>{
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    })
+})
+
+server.listen(8000, ()=>{
+    console.log('8000포트에서 실행');
+})
