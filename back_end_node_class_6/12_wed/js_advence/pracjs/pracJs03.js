b3bbf18bd19228d687df5a4337e03e528d833f48
bigkoooo <kimminseok8910@gmail.com>
2024-06-13 11:29:53 +0900

    clamberUp

--- back_end_node_class_6/12_wed/js_advence/pracjs/pracJs03.js
+++ back_end_node_class_6/12_wed/js_advence/pracjs/pracJs03.js
@@ -12,18 +12,55 @@ class Shape {
 
 
 let rec1 = new Shape(3,4);
-console.log(rec1.getArea());
+// console.log(rec1.getArea());
 
 
 // 클래스의 상속
-class Triangle extends Shape{
-    constructor(width,height,divide){
-        super(width,height,divide)
-        this.divide =divide;
+class Rectangle extends Shape{
+    constructor(width,height){
+        super(width,height)
+        
         
     }
-    getTriangle() {
-        
+    getDiagonal() {
+        return Math.sqrt(this.height**2+this.width**2)
       }
 
 }
+
+const rec2 = new Rectangle(3,4);
+
+console.log(rec2.getArea());
+console.log(rec2.getDiagonal());
+
+// 클래스의 상속 삼각형 넓이 구하기
+class Triangle extends Shape{
+    constructor(width,height){
+        super(width,height)
+        
+        
+    }
+    getArea() {
+        return (this.height * this.width) / 2
+      }
+
+}
+
+const tri= new Triangle(3,4);
+console.log(tri.getArea());
+
+// 클래스의 상속 원 넓이 구하기
+class Oval extends Shape{
+    constructor(width,height){
+        super(width,height)
+        
+        
+    }
+    getArea() {
+            return Math.PI * this.width/2 *this.height/2
+    }
+
+}
+
+const circle= new Oval(3,4);
+console.log(circle.getArea());
