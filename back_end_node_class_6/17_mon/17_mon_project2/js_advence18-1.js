01144f302ff2ee45e4c420ed5e45afc117df6933
bigkoooo <kimminseok8910@gmail.com>
2024-06-20 21:46:43 +0900

    clamberUp

--- back_end_node_class_6/17_mon/17_mon_project2/js_advence18-1.js
+++ back_end_node_class_6/17_mon/17_mon_project2/js_advence18-1.js
@@ -1,4 +1,5 @@
 const { log } = require("console");
+const e = require("express");
 const express = require("express");
 const multer = require('multer');
 const path = require('path');
@@ -12,7 +13,10 @@ app.set('views', './vvviews');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use('/uploads', express.static(path.join(__dirname,'uploads'))); // 중복된 설정 제거
+// app.use('/uploads', express.static(path.join(__dirname + '/uploads'))); // 이 줄 삭제
+app.use('/static', 
+    express.static(__dirname + '/public'));
 
 const uploadDetail = multer({
     storage: multer.diskStorage({
@@ -31,7 +35,7 @@ app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
     console.log(req.body);
     console.log(req.file);
 
-    res.render('js_advence20', { title: req.body.title, src: req.file.path });
+    res.render('js_advence20', { title: req.body.title, src: req.file.filename }); // req.file.filename으로 변경
 });
 
 app.post('/upload/array', uploadDetail.array('banana'), (req, res) => {
@@ -47,6 +51,11 @@ app.post('/upload/fields', uploadDetail.fields([{ name: 'kiwi' }, { name: 'orang
 
     res.send('Success Upload!! (multiple2)');
 });
+
+// 동적 폼 업로드
+app.post('/dynamicFile', uploadDetail.single('thumbnail'), (req, res) => {
+    res.send(req.file);
+})
 
 app.get('/', function (req, res) {
     res.render('js_advence19-1', { title: '파일 업로드를 배워보자!!!' });
--- back_end_node_class_6/17_mon/17_mon_project2/public/js/thumbnail.js
+++ back_end_node_class_6/17_mon/17_mon_project2/public/js/thumbnail.js
@@ -23,6 +23,16 @@ function uploadThumbnail() {
         headers: {
             'Content-Type': 'multipart/form-data', // enctype="multipart/form-data"
         }
+    }).then(function (res) {
+        // 서버의 req.file을 클라이언트의 res 변수가 받음
+        console.log(res);
+        console.log(res.data.path); // 이미지 주소
+
+        // img태그의 src 속성값에 이미지 주소를 부여
+        document.querySelector('img').src =`/${res.data.path}`;
+
+        // img태그에 thumbnail 클래스명 부여
+        document.querySelector('img').classList.add('thumbnail');
     })
 
 }
