f4bb5656acf9bf1381e6d80005bb6d600bb9acf9
bigkoooo <kimminseok8910@gmail.com>
2024-06-20 15:02:53 +0900

    keepGoing

--- back_end_node_class_6/19_wed/19_wed_projectClass/js_advence21-1.js
+++ back_end_node_class_6/19_wed/19_wed_projectClass/js_advence21-1.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
-const PORT = 8080;
+const PORT = 8888;
 
 app.set('view engine', 'ejs');
-app.set('views', './vvviews'); 
+app.set('views', './vvviews');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.render('js_advence22', { title: '동적 폼을 배워보자!' });
+    res.render('js_advence22-1', { title: '어차피 하루 뒤면 다까먹고 기억1도 못할텐데...ㅠ' });
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const storage = multer.diskStorage({
     destination(req, file, done) {
-        done(null, 'uploads/'); 
+        done(null, 'uploads/');
     },
     filename(req, file, done) {
         const ext = path.extname(file.originalname);
@@ -28,23 +29,20 @@ const storage = multer.diskStorage({
 });
 
 const uploadDetail = multer({
-    storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 } 
+    storage,
+    limits: { fileSize: 10 * 1024 * 1024 }
 });
 
-app.post("/upload", uploadDetail.single('userfile'), (req, res) => {
-    console.log(req.body); 
-    console.log(req.file); 
-
+app.post("/stealme", uploadDetail.single('userfile'), (req, res) => {
     const userInfo = {
-        urID: req.body.urID || 'Unknown ID',
-        password: req.body.password || 'Unknown Password',
-        name: req.body.name || 'Unknown Name',
-        urAge: req.body.urAge || 'Unknown Age',
+        urID: req.body.urID,
+        password: req.body.password,
+        name: req.body.name,
+        urAge: req.body.urAge,
     };
 
-    res.render('js_advence23', { 
-        title: req.body.title, 
+    res.render('js_advence23-1', {
+        title: req.body.title,
         src: `/uploads/${req.file.filename}`,
         userInfo: userInfo
     });
