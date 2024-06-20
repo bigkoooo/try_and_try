bb3e38e70630419615876271acf9b382f2c7ff00
bigkoooo <kimminseok8910@gmail.com>
2024-06-20 23:12:17 +0900

    keepgoing

--- back_end_node_class_6/17_mon/17_mon_project2/js_advence18-1.js
+++ back_end_node_class_6/17_mon/17_mon_project2/js_advence18-1.js
@@ -1,22 +1,17 @@
-const { log } = require("console");
-const e = require("express");
 const express = require("express");
 const multer = require('multer');
 const path = require('path');
-const { title } = require("process");
 
 const app = express();
-const PORT = 8888;
+const PORT = 8000;
 
 app.set('view engine', 'ejs');
 app.set('views', './vvviews');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/uploads', express.static(path.join(__dirname,'uploads'))); // 중복된 설정 제거
-// app.use('/uploads', express.static(path.join(__dirname + '/uploads'))); // 이 줄 삭제
-app.use('/static', 
-    express.static(__dirname + '/public'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/static', express.static(path.join(__dirname, 'public'))); // public 디렉토리 설정
 
 const uploadDetail = multer({
     storage: multer.diskStorage({
@@ -35,7 +30,7 @@ app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
     console.log(req.body);
     console.log(req.file);
 
-    res.render('js_advence20', { title: req.body.title, src: req.file.filename }); // req.file.filename으로 변경
+    res.render('js_advence20', { title: req.body.title, src: req.file.filename });
 });
 
 app.post('/upload/array', uploadDetail.array('banana'), (req, res) => {
@@ -54,8 +49,8 @@ app.post('/upload/fields', uploadDetail.fields([{ name: 'kiwi' }, { name: 'orang
 
 // 동적 폼 업로드
 app.post('/dynamicFile', uploadDetail.single('thumbnail'), (req, res) => {
-    res.send(req.file);
-})
+    res.json({ filename: req.file.filename });
+});
 
 app.get('/', function (req, res) {
     res.render('js_advence19-1', { title: '파일 업로드를 배워보자!!!' });
--- back_end_node_class_6/17_mon/17_mon_project2/public/js/thumbnail.js
+++ back_end_node_class_6/17_mon/17_mon_project2/public/js/thumbnail.js
@@ -1,38 +1,28 @@
-console.log('!')
+console.log('!');
 
 function uploadThumbnail() {
-    // FormData 객체
-    // : 폼 전송할 때, 멀티미디어 파일을 비동기로 제출하고 싶다면.. FormData 객체를 이용
     const formData = new FormData();
 
-    // 폼 요소를 선택
     const fileInput = document.querySelector('#thumbnail');
     console.dir(fileInput);
-    console.dir(fileInput.files); // 업로드한 파일 정보
+    console.dir(fileInput.files);
 
-    // FormData에 업로드한 파일 정보를 추가
     formData.append('thumbnail', fileInput.files[0]);
-    // console.log(formData); // FormData는 특수한 형태의 객체이기 때문에 쉽게 출력 x
 
-    // 서버로 요청 보내기
     axios({
         method: 'POST',
         url: '/dynamicFile',
         data: formData,
-        // key
         headers: {
-            'Content-Type': 'multipart/form-data', // enctype="multipart/form-data"
+            'Content-Type': 'multipart/form-data',
         }
     }).then(function (res) {
-        // 서버의 req.file을 클라이언트의 res 변수가 받음
         console.log(res);
-        console.log(res.data.path); // 이미지 주소
+        console.log(res.data.filename); 
 
-        // img태그의 src 속성값에 이미지 주소를 부여
-        document.querySelector('img').src =`/${res.data.path}`;
-
-        // img태그에 thumbnail 클래스명 부여
+        document.querySelector('img').src = `/uploads/${res.data.filename}`;
         document.querySelector('img').classList.add('thumbnail');
-    })
-
+    }).catch(function (error) {
+        console.error('Error:', error);
+    });
 }
