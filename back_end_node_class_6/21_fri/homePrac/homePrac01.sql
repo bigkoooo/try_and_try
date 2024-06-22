2b1a11cdbacb3c819044838fb20a5737a31e8196
bigkoooo <kimminseok8910@gmail.com>
2024-06-22 15:48:43 +0900

    keepGoing

---
+++ back_end_node_class_6/21_fri/homePrac/homePrac01.sql
@@ -0,0 +1,13 @@
+-- 데이터베이스 : 데이터의 집합
+-- DBMS: Database Management Syetem, 데이터 베이스를 운영/관리하는 프로그램
+    -- ex. MySQL
+-- SQL: 데이터베이스를 구축, 관리하고 활용하기 위해 사용하는 언어
+-- 참고) 스키마 == 데이터 베이스
+
+-- DDL(Data Definition Language)
+-- : 데이터베이스, 테이블을 "정의"하는 언어
+
+-- [데이터베이스] 관련 명령어
+-- 1. 데이터베이스 생성
+
+create Database cordingon character set utf8mb4 collate utf8mb4_unicode_ci;
