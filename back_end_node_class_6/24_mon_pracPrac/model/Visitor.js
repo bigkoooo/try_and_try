75080472935c86de8dafa3c9dd0704fcfd4fc03f
bigkoooo <kimminseok8910@gmail.com>
2024-06-26 18:08:41 +0900

    clamberUp

--- back_end_node_class_6/24_mon_pracPrac/model/Visitor.js
+++ back_end_node_class_6/24_mon_pracPrac/model/Visitor.js
@@ -12,10 +12,10 @@ const mysql = require('mysql');
 
 const conn = mysql.createConnection({
   host: 'localhost',
-  user: 'user1',
-  password: '12345678',
+  user: 'user',
+  password: '1234',
   database: 'codingon'
-}); // database 연결 객체
+}); // database 연결 객체ㅠ
 
 exports.getVisitors = (callback) => {
   conn.query(`select * from visitor`, (err, rows) => {
@@ -27,6 +27,18 @@ exports.getVisitors = (callback) => {
     callback(rows)
   })
 };    
+
+exports.getVisitor = (targetId, callback) => {
+  conn.query(`select * from visitor where id=${targetId}`, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('model/Visitor.js >> ', rows);
+    // model/Visitor.js >>  [ RowDataPacket { id: 6, name: 'lily', comment: 'hello' } ]
+    callback(rows[0])  
+  })
+}
 
 exports.postVisitor = (data, callback) => {
   conn.query(`insert into visitor(name, comment) values ('${data.name}', '${data.comment}')`, 
@@ -49,4 +61,40 @@ exports.postVisitor = (data, callback) => {
       callback(rows.insertId)
     }
   )
+}
+
+exports.deleteVisitor = (targetId, callback) => {
+  // targetId: 삭제해야할 visitor id
+  conn.query(`delete from visitor where id=${targetId}`, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('model/Visitor.js >> ', rows);
+    // OkPacket {
+    //   fieldCount: 0,
+    //   affectedRows: 1,
+    //   insertId: 0,
+    //   serverStatus: 2,
+    //   warningCount: 0,
+    //   message: '',
+    //   protocol41: true,
+    //   changedRows: 0
+    // }
+    callback(true); // 삭제
+  })
+}
+
+exports.patchVisitor = (updateData, callback) => {
+  const { id, name, comment } = updateData;
+  conn.query(
+    `update visitor set name='${name}', comment='${comment}' where id=${id}`,
+    (err, rows) => {
+      if (err) {
+        throw err;
+      }
+
+      console.log('model/Visitor.js >> ', rows);
+      callback(true); // 수정
+    });
 }
