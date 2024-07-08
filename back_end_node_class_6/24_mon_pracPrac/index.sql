b407e7863d6378ca358296fb2f89531f46e57e97
bigkoooo <kimminseok8910@gmail.com>
2024-07-08 14:22:44 +0900

    pracSoket

--- back_end_node_class_6/24_mon_pracPrac/index.sql
+++ back_end_node_class_6/24_mon_pracPrac/index.sql
@@ -18,10 +18,15 @@ insert into visitor values
 select * from visitor;
 
 -- user 계정 생성
-create user 'user1'@'%' identified by '12345678'; -- 계정 추가
-grant all privileges on *.* to 'user1'@'%' with grant option; -- 새로운 계정에 권한 부여
+create user 'test1'@'localhost' identified by '12345678'; -- 계정 추가
+grant all privileges on *.* to 'test1'@'localhost' with grant option; -- 새로운 계정에 권한 부여
 flush privileges; -- 캐시 지우고 새로운 설정 적용
 
-alter user 'user1'@'%' identified with mysql_native_password by '12345678'; -- 인증 방식 변경
+alter user 'test1'@'localhost' identified with mysql_native_password by '12345678Profile'; -- 인증 방식 변경
 
-select * from mysql.user; -- 계정 생성 확인
+drop user 'user1';
+select host, user, plugin from mysql.user; -- 계정 생성 확인
+
+desc player;
+desc Profile;
+desc Team;
